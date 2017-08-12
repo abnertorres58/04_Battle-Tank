845979353bfd48b086924618b564701184de4fa8
Freud Romero <abnertorres58@gmail.com>
2017-08-11 20:02:27 -0400

    BT26 Adding Tick() to player controller

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,7 @@
 
 #include "TankPlayerController.h"
 
+
 void ATankPlayerController::BeginPlay()
 {
     Super::BeginPlay();
@@ -18,9 +19,31 @@ void ATankPlayerController::BeginPlay()
     }
 }
 
+
+//Tick
+void ATankPlayerController::Tick(float DeltaTime)
+{
+    //Super
+    Super::Tick(DeltaTime);
+    
+    //UE_LOG(LogTemp, Warning, TEXT("Player Controller ticking"))
+    
+    AimTowardsCrosshair();
+    
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+    if(!GetControlledTank()) {return;}
+    
+    //Get world location if linetrace trough crosshair
+    //If it hits the lanscape
+        //Tell the controller to aim to this point
 }
 
 
